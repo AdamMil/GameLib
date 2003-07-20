c1d1f425504c065d88ba0b2eb0e3ccc83e82d728
Adam Milazzo <adam@adammil.net>
2003-07-20 10:28:33 +0000

    Added Linked List and fixed some bugs Made TrueTypeFont class use the RedBlackTree and LinkedList for a cache Finished the priority queue (though it wasn't needed)

---
+++ trunk/LinkedList.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+
+namespace GameLib.Collections
+{
+
+public class LinkedList : ICollection, IEnumerable
+{ public LinkedList() { cmp=Comparer.Default; }
+  public LinkedList(IComparer comparer) { cmp=comparer; }
+  public LinkedList(ICollection col) { foreach(object o in col) Append(o); }
+  public LinkedList(IComparer comparer, ICollection col)
+  { cmp=comparer;
+    foreach(object o in col) Append(o);
+  }
+
+  public class Node
+  { internal Node(object data) { this.data=data; }
+    internal Node(object data, Node prev, Node next) { this.data=data; Prev=prev; Next=next; }
+    public object Data { get { return data; } }
+    internal Node Prev, Next;
+    object data;
+  }
+
+  #region ICollection
+  public int    Count          { get { return count; } }
+  public bool   IsSynchronized { get { return false; } }
+  public object SyncRoot       { get { return this; } }
+  
+  public void CopyTo(Array array, int startIndex)
+  { Node n = head;
+    while(head!=null) array.SetValue(head.Data, startIndex++);
+  }
+  #endregion
+
+  #region IEnumerable
+  public class Enumerator : IEnumerator
+  { internal Enumerator(LinkedList list, Node head)
+    { if(head==null) GC.SuppressFinalize(this);
+      else
+      { handler = new ListChangeHandler(OnListChanged);
+        list.ListChanged += handler;
+        this.list=list;
+      }
+      this.head=head;
+      Reset();
+    }
+    ~Enumerator() { list.ListChanged -= handler; }
+
+    public object Current
+    { get
+      { if(cur==null) throw new InvalidOperationException("Invalid position");
+        return cur.Data;
+      }
+    }
+    
+    public bool MoveNext()
+    { if(changed) throw new InvalidOperationException("The collection has changed");
+      if(cur==null)
+      { if(!reset && head==null) return false;
+        cur   = head;
+        reset = false;
+        return true;
+      }
+      cur = cur.Next;
+      return cur!=null;
+    }
+
+    public void Reset()
+    { cur=null; reset=true;
+    }
+    
+    void OnListChanged()
+    { changed=true;
+      list.ListChanged -= handler;
+      GC.SuppressFinalize(this);
+    }
+
+    ListChangeHandler handler;
+    LinkedList list;
+    Node  head, cur;
+    bool  reset, changed;
+  }
+  
+  IEnumerator IEnumerable.GetEnumerator() { return new Enumerator(this, head); }
+
+  internal delegate void ListChangeHandler();
+  internal event ListChangeHandler ListChanged;
+  #endregion
+  
+  public Node Head  { get { return head; } }
+  public Node Tail  { get { return tail; } }
+
+  public Node Append(object o)  { return tail==null ? head=tail=new Node(o) : InsertAfter(tail, o); }
+  public Node Prepend(object o) { return head==null ? head=tail=new Node(o) : InsertBefore(head, o); }
+  public Node InsertAfter(object at, object o)
+  { Node n = Find(at);
+    if(n==null) throw new ArgumentException("Object not found in list", "at");
+    return InsertAfter(n, o);
+  }
+  public Node InsertBefore(object at, object o)
+  { Node n = Find(at);
+    if(n==null) throw new ArgumentException("Object not found in list", "at");
+    return InsertBefore(n, o);
+  }
+  public void Remove(object o) { Remove(Find(o)); }
+  public bool Contains(object o) { return Find(o)!=null; }
+
+  public Node Find(object o) { return Find(o, head); }
+  public Node Find(object o, Node start)
+  { while(start!=null && cmp.Compare(start.Data, o)!=0) start=start.Next;
+    return start;
+  }
+  public Node FindLast(object o) { return Find(o, tail); }
+  public Node FindLast(object o, Node end)
+  { while(end!=null && cmp.Compare(end.Data, o)!=0) end=end.Prev;
+    return end;
+  }
+  public Node InsertAfter(Node node, object o)
+  { if(node==null) throw new ArgumentNullException("node");
+    node.Next = new Node(o, node, node.Next);
+    if(node==tail) tail=node.Next;
+    count++;
+    if(ListChanged!=null) ListChanged();
+    return node.Next;
+  }
+  public Node InsertBefore(Node node, object o)
+  { if(node==null) throw new ArgumentNullException("node");
+    node.Prev = new Node(o, node.Prev, node);
+    if(node==head) head=node.Prev;
+    count++;
+    if(ListChanged!=null) ListChanged();
+    return node.Prev;
+  }
+  public void Remove(Node node)
+  { if(node==null) return;
+    if(node.Prev!=null) node.Prev.Next=node.Next;
+    if(node.Next!=null) node.Next.Prev=node.Prev;
+    if(node==head) head=node.Next;
+    if(node==tail) tail=node.Prev;
+    count--;
+    if(ListChanged!=null) ListChanged();
+  }
+  public void Clear() { head=tail=null; }
+  
+  protected IComparer cmp;
+  protected Node head, tail;
+  protected int count;
+}
+
+} // namespace GameLib.Collections
--- trunk/PriorityQueue.cs
+++ trunk/PriorityQueue.cs
@@ -4,8 +4,50 @@ using System.Collections;
 namespace GameLib.Collections
 {
 
-public class RedBlackTree : ICloneable, IDictionary, ICollection, IEnumerable
+public class PriorityQueue : RedBlackTree
 { 
+  #region IDictionary
+  public new object this[object key]
+  { get { CheckKey(key); return base[key]; }
+    set { CheckKey(key); base[key]=value; }
+  }
+  
+  public new void Add(object key, object value)
+  { CheckKey(key);
+    base.Add(key, value);
+  }
+  #endregion
+
+  public void Add(int priority, object value) { base.Add(priority, value); }
+
+  public object RemoveMinimum()
+  { if(count==0) throw new InvalidOperationException("The collection is empty");
+    Node n=root;
+    do n=n.Left; while(n.Left!=Node.Null);
+    Remove(n);
+    return n.Value;
+  }
+
+  public object RemoveMaximum()
+  { if(count==0) throw new InvalidOperationException("The collection is empty");
+    Node n=root;
+    do n=n.Right; while(n.Right!=Node.Null);
+    Remove(n);
+    return n.Value;
+  }
+
+  public void SetPriority(int oldPriority, int newPriority)
+  { if(Contains(newPriority))
+      throw new ArgumentException("Tree already contains the priority "+newPriority, "newPriority");
+    Node n = Find(oldPriority);
+    if(n==Node.Null) throw new ArgumentException("The old priority does not exist in the tree", "oldPriority");
+    Remove(n);
+    Add(newPriority, n.Value);
+  }
+
+  protected void CheckKey(object key)
+  { if(!(key is int)) throw new ArgumentException("Key must be an integer");
+  }
 }
 
 } // namespace GameLib.Collections
