eb4f779951d1bbf50b895f33dba6c238450a2bf4
adam <adam@54c6ee85-54ec-0310-a778-f7c3feacb35f>
2003-07-25 23:59:41 +0000

    Added code to do format conversion to the Mixer library, but didn't test it

--- trunk/Mixer/Mixer.h
+++ trunk/Mixer/Mixer.h
@@ -10,6 +10,16 @@
 extern "C" {
 #endif
 
+typedef struct
+{ byte  *buf;
+  int    len;
+  int    srcRate, destRate;
+  int    len_cvt;
+  int    len_mul, len_div;
+  ushort srcFormat, destFormat;
+  byte   srcChans,  destChans;
+} GLM_AudioCVT;
+
 typedef void (__stdcall *MixCallback)(Sint32 *stream, Uint32 samples, void *context);
 
 extern DECLSPEC int  SDLCALL GLM_Init(Uint32 freq, Uint16 format, Uint8 channels, Uint32 bufferMs,
@@ -20,10 +30,13 @@ extern DECLSPEC void SDLCALL GLM_Quit();
 extern DECLSPEC Uint16 SDLCALL GLM_GetMixVolume();
 extern DECLSPEC void   SDLCALL GLM_SetMixVolume(Uint16 volume);
 
+extern DECLSPEC int SDLCALL GLM_ConvertAcc(void *dest, Sint32 *src, Uint32 samples, Uint16 destFormat);
+extern DECLSPEC int SDLCALL GLM_SetupCVT(GLM_AudioCVT *cvt, int inputLen);
+extern DECLSPEC int SDLCALL GLM_Convert(GLM_AudioCVT *cvt);
+
 extern DECLSPEC int SDLCALL GLM_Copy(Sint32 *dest, Sint32 *src, Uint32 samples);
 extern DECLSPEC int SDLCALL GLM_VolumeScale(Sint32 *stream, Uint32 samples, Uint16 volume);
 extern DECLSPEC int SDLCALL GLM_Mix(Sint32 *dest, Sint32 *src, Uint32 samples, Uint16 srcVolume);
-extern DECLSPEC int SDLCALL GLM_ConvertAcc(void *dest, Sint32 *src, Uint32 samples, Uint16 destFormat);
 extern DECLSPEC int SDLCALL GLM_ConvertMix(Sint32 *dest, void *src, Uint32 samples,
                                            Uint16 srcFormat, Uint16 srcVolume);
 extern DECLSPEC int SDLCALL GLM_DivideAccumulator(Sint32 divisor);
