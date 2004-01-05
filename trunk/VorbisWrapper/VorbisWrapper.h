59cd7a58a74ba8d15fa655e9c0094014c0c7e294
Adam Milazzo <adam@adammil.net>
2004-01-05 01:15:19 +0000

    Added GPL header Fixed some bugs in IOH

--- trunk/VorbisWrapper/VorbisWrapper.h
+++ trunk/VorbisWrapper/VorbisWrapper.h
@@ -1,3 +1,21 @@
+/*
+GameLib is a library for developing games and other multimedia applications.
+http://www.adammil.net/
+Copyright (C) 2002-2004 Adam Milazzo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 #ifndef GAMELIB_VORBIS_H
 #define GAMELIB_VORBIS_H
 
@@ -11,6 +29,7 @@ extern "C" {
 
 enum { ALLSECTIONS=-1 };
 
+// FIXME: HACK: change to SDLCALL for release
 typedef struct
 { Sint32 (__stdcall *Read)(void *context, void *dest, Sint32 size, Sint32 maxnum); // TODO: change to SDLCALL
   Sint32 (__stdcall *Seek)(void *context, Sint32 offset, int whence);
