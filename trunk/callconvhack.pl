b5917fd6ef450e366ef282148c945070d7e0377f
adam <adam@54c6ee85-54ec-0310-a778-f7c3feacb35f>
2004-12-25 22:49:00 +0000

    *** empty log message ***

--- trunk/callconvhack.pl
+++ trunk/callconvhack.pl
@@ -3,10 +3,10 @@ while(<>)
 { if(/^\.module extern SDL_image$/) { s/SDL_image/SDL/ }
   elsif(/^\.module extern SDL$/) { s/SDL/SDL_image/ }
   if (/\.class .* CallConv.*Attribute/) { $incallconvattributeimpl=1; next; }
-  if(/\/\/ end of class CallConv.*Attribute/) { $incallconvattributeimpl=0; next; }
+  if(/\/\/ end of class .*CallConv.*Attribute/) { $incallconvattributeimpl=0; next; }
   if(/\.custom instance void .*CallConv(.*)Attribute/) { $pcc = $1; next; }
   if ($pcc)
-  { if(m/.*Invoke\(.*/)
+  { if(m/^\s*(?:instance default [\w\.]+ )?Invoke\s*\(/)
     { print "modopt([mscorlib]System.Runtime.CompilerServices.CallConv$pcc)\n";
       $pcc = 0;
     }
