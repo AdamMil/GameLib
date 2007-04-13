d83dbf6074f56250cb990eeb219886283b0d53e7
adam <adam@54c6ee85-54ec-0310-a778-f7c3feacb35f>
2007-04-13 20:58:10 +0000

    Attempting to eliminate the 'trunk' directory.

--- branches/NET-2.0/callconvhack.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Program Files\Microsoft Visual Studio 8\SDK\v2.0\Bin\ildasm.exe" /source /nobar /out:temp.il "%1\bin\%2\%3.dll"
-perl "%1\callconvhack.pl" temp.il >hacked.il
-"%windir%\Microsoft.NET\Framework\v2.0.40607\ilasm.exe" /quiet /dll /debug /out:"%1\bin\%2\%3.dll" /res:temp.res hacked.il >nul
-copy /Y "%1\bin\%2\%3.dll" "%1\obj\%2\%3.dll"
-copy /Y "%1\bin\%2\%3.pdb" "%1\obj\%2\%3.pdb"
-del temp.il temp.res
